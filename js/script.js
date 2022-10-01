e6e341a2100c97ea65e6d079b87d88ad8c86fef1
OrelA884 <114829934+OrelA884@users.noreply.github.com>
2022-10-01 21:49:12 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ================================ typing animation =================================== */
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer","Youtuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
